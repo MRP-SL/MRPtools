739f13055294d32980588fae68f2512f03b35a7a
Jordan Imahori <jordanimahori@gmail.com>
2023-09-01 20:39:13 +0000

    Add functions for loading directory of Excel and delim files

---
+++ R/load_delim_directory.R
@@ -0,0 +1,39 @@
+#' load_delim_directory
+#' @description
+#' Read and append all delimited files in a directory. Use ... to pass additional arguments to readr::read_delim(), which is used under the hood.
+#'
+#' @param dir The directory in which to load all files.
+#' @param delim Single character used to separate fields within a record.
+#' @param ... (Optional) To pass additional arguments to read_excel().
+#'
+#' @return A tibble containing the merged observations from the selected sheets.
+#' @export
+#'
+#' @examples
+load_delim_directory <- function(dir, delim, ...) {
+    # Read and append all Excel files in a directory using readr::read_excel().
+    # Sheets specifies which sheets in each of the Excel files within the
+    # directory should be used. Use ... to pass additional arguments to read_excel.
+
+    require(readr, quietly = TRUE)
+    require(dplyr, quietly = TRUE)
+
+    stopifnot("the supplied directory does not exist" = dir.exists(dir))
+    stopifnot("delim must be a single character" = length(delim) == 1 & nchar(delim) == 1)
+
+    filepaths <- list.files(dir, full.names = TRUE)
+
+    for (file in filepaths) {
+        if (exists("output")) {
+            output <- dplyr::bind_rows(
+                output,
+                readr::read_delim(file, delim = delim, col_types = "character", ...)
+            )
+        } else {
+            output <- readr::read_delim(file, delim = delim, col_types = "character", ...)
+        }
+    }
+
+    readr::type_convert(output)
+
+}
---
+++ R/load_excel_directory.R
@@ -0,0 +1,35 @@
+#' load_excel_directory
+#'
+#'@description
+#' Read and append all Excel files in a directory. Optionally, you can specify which sheets should be loaded using a vector of indices of sheet names. Use ... to pass additional arguments to readxl::read_excel(), which is used under the hood.
+#'
+#' @param dir The directory in which to load all files.
+#' @param sheets (Optional) A numeric vector of indices or a character vector of names for the sheets to use.
+#' @param ... (Optional) To pass additional arguments to read_excel().
+#'
+#' @return A tibble containing the merged observations from the selected sheets.
+#' @export
+#'
+#' @examples
+load_excel_directory <- function(dir, sheets = "all", ...) {
+
+    require(dplyr, quietly = TRUE)
+
+    stopifnot("the supplied directory does not exist" = dir.exists(dir))
+
+    filepaths <- list.files(dir, full.names = TRUE)
+
+    for (file in filepaths) {
+        if (exists("output")) {
+            output <- dplyr::bind_rows(
+                output,
+                concat_excel_sheets(file, sheets = sheets, ...)
+            )
+        } else {
+            output <- concat_excel_sheets(file, sheets = sheets, ...)
+        }
+    }
+
+    readr::type_convert(output)
+
+}
